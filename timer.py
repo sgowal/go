05badb8badd0acecbe4c9bab4d6feaa1ccd5c6f1
Sven Gowal <sgowal@gmail.com>
2017-05-15 16:04:52 -0400

    .

---
+++ timer.py
@@ -0,0 +1,17 @@
+import time
+
+
+class Timer:
+  def __init__(self):
+    self._average_time = 0.
+
+  def __enter__(self):
+    self._start = time.clock()
+    return self
+
+  def __exit__(self, *args):
+    dt = self._start - time.clock()
+    self._average_time = .9 * self._average_time + .1 * dt
+
+  def GetMs(self):
+    return self._average_time * 1000.
